b8785a5a5b6df645228e9a55602615a198362286
TraceofLight <rickyjun96@gmail.com>
2025-10-14 22:02:51 +0900

    misc: build warning
    빌드 경고들 일괄 제거
    - 멀티 프로세스 빌드를 활용하여 빌드 속도 향상

--- Engine/Source/Editor/Private/Editor.cpp
+++ Engine/Source/Editor/Private/Editor.cpp
@@ -526,7 +526,7 @@ void UEditor::ProcessMouseInput()
 				FScopeCycleCounter PickCounter;
 				UPrimitiveComponent* PrimitiveCollided = ObjectPicker.PickPrimitive(CurrentCamera, WorldRay, Candidate, &ActorDistance);
 				ActorPicked = PrimitiveCollided ? PrimitiveCollided->GetOwner() : nullptr;
-				float ElapsedMs = PickCounter.Finish(); // 피킹 시간 측정 종료
+				float ElapsedMs = static_cast<float>(PickCounter.Finish()); // 피킹 시간 측정 종료
 				UStatOverlay::GetInstance().RecordPickingStats(ElapsedMs);
 			}
 		}
